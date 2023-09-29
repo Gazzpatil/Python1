a5f81d3c88a2541e57c0cb00ef412701ad82924c
Gajanand Patil <116052536+Gazzpatil@users.noreply.github.com>
2023-09-29 23:30:54 +0530

    first python program

---
+++ Automorphic_num.py
@@ -0,0 +1,17 @@
+
+# Automorphic number 
+# a=5 -- output b=25  -->the output is ending similar to the input 5
+
+
+num=int(input("enter the number:"))
+gazz=str(num) 
+
+num1=num**2
+ash=str(num1)
+
+if ash.endswith(gazz):
+    print("the number is Automorphic ")
+
+else:
+    print("the number is not automorphic ")    
+
